2aed5fe2319d9820a63835e5cae57b1d226f3363
PKfede <47314404+pkfede@users.noreply.github.com>
2019-12-13 16:33:50 -0600

    prueba de agregar pacientes con activity

--- PhyigitalApp/app/src/main/java/com/modelo/phygital/ui/pacientes/PacientesFragment.kt
+++ PhyigitalApp/app/src/main/java/com/modelo/phygital/ui/pacientes/PacientesFragment.kt
@@ -15,6 +15,7 @@ import com.google.firebase.database.ChildEventListener
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
+import com.modelo.phygital.DemoAdapterPaciente
 
 
 data class User(var firstName: String = "", var lastName: String = "", var age: Int = 0, var state: String = "") {
@@ -37,69 +38,7 @@ data class User(var firstName: String = "", var lastName: String = "", var age: 
 
 }
 
-class DemoAdapterPaciente(private val users: ArrayList<User>) : RecyclerView.Adapter<DemoAdapterPaciente.DemoViewHolder>() {
 
-
-    class DemoViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
-        private var tvFullName: TextView
-        private var tvAge: TextView
-        private var tvState: TextView
-        private var btnDelete: ImageButton
-        private var btnEdit: ImageButton
-
-
-        init {
-            tvFullName = view.findViewById(R.id.full_name)
-            tvAge = view.findViewById(R.id.age)
-            tvState = view.findViewById(R.id.state)
-            btnDelete = view.findViewById(R.id.delete_button)
-            btnEdit = view.findViewById(R.id.edit_button)
-
-
-
-        }
-
-        public fun bind(user: User) {
-            tvFullName.setText("${user.lastName}, ${user.firstName}")
-            tvAge.setText("${user.age}")
-            tvState.setText("${user.state}")
-            val database = FirebaseDatabase.getInstance()
-            val usersRef = database.getReference("app").child("users")
-
-            btnDelete.setOnClickListener{
-
-                usersRef.child(user.id!!).removeValue()
-                val snack = Snackbar.make(it,"Paciente eliminado con exito",Snackbar.LENGTH_LONG)
-
-            }
-            btnEdit.setOnClickListener{
-                val snack = Snackbar.make(it,"Esto sera para editar, aun no se como saldra",Snackbar.LENGTH_LONG)
-                snack.show()
-            }
-
-        }
-
-
-
-    }
-
-
-    override fun onCreateViewHolder(
-        parent: ViewGroup,
-        viewType: Int
-    ): DemoViewHolder {
-        val view = LayoutInflater.from(parent.context)
-            .inflate(R.layout.rv_demo_holder_paciente, parent, false) as View
-
-        return DemoViewHolder(view)
-    }
-
-    override fun onBindViewHolder(holder: DemoViewHolder, position: Int) {
-        holder.bind(users[position])
-    }
-
-    override fun getItemCount() = users.size
-}
 private lateinit var rv: RecyclerView
 
 
@@ -124,7 +63,7 @@ class HomeFragment : Fragment() {
     ): View? {
         pacientesViewModel =
             ViewModelProviders.of(this).get(PacientesViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_pacientes, container, false)
+        val root = inflater.inflate(R.layout.activity_add_pacientes, container, false)
 
         etFirstName = root.findViewById(R.id.frame_text)
         etLastName = root.findViewById(R.id.lname_text)
